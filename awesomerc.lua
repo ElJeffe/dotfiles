2c96c806f09e44fddc36271387618cb5c711901b
steelj99 <jef@steelant.be>
2009-02-20 17:03:05 +0100

    update

--- awesomerc.lua
+++ awesomerc.lua
@@ -126,9 +126,55 @@ cpuwidget:plot_properties_set( "cpu", {
 })
 wicked.register(cpuwidget, wicked.widgets.cpu, '$1', 1, "cpu")
 
+
+
 -- date widget
 datewidget = widget({ type = "textbox", name = "datewidget", align="right"})
-wicked.register(datewidget, wicked.widgets.date, " <span color='white'>Date:</span> %c")
+wicked.register(datewidget, wicked.widgets.date, "%d.%m %H:%M")
+
+-- battery widget
+batteries = 1
+
+-- Function to extract charge percentage
+function read_battery_life(number)
+   return function(format)
+             local fh = io.popen('acpi')
+             output = fh:read("*a")
+             fh:close()
+
+             count = 0
+             for s in string.gmatch(output, "(%d+)%%") do
+                if number == count then
+                   return {s}
+                end
+                count = count + 1
+             end
+          end
+end
+
+-- Display one vertical progressbar per battery
+for battery=0, batteries-1 do
+   batterygraphwidget = widget({ type = 'progressbar',
+                                 name = 'batterygraphwidget',
+                                 align = 'right' })
+   batterygraphwidget.height = 0.85
+   batterygraphwidget.width = 8
+   batterygraphwidget.bg = '#333333'
+   batterygraphwidget.border_color = '#0a0a0a'
+   batterygraphwidget.vertical = true
+   batterygraphwidget:bar_properties_set('battery',
+                                         { fg = '#AEC6D8',
+                                           fg_center = '#285577',
+                                           fg_end = '#285577',
+                                           fg_off = '#222222',
+                                           vertical_gradient = true,
+                                           horizontal_gradient = false,
+                                           ticks_count = 0,
+                                           ticks_gap = 0 })
+
+   wicked.register(batterygraphwidget, read_battery_life(battery), '$1', 1, 'battery')
+end
+
 
 -- Create a wibox for each screen and add it
 mywibox = {}
@@ -173,8 +219,9 @@ for s = 1, screen.count() do
                            mytasklist[s],
                            mypromptbox[s],
                            -- mytextbox,
-			   datewidget,
-			   cpuwidget,
+                           datewidget,
+                           cpuwidget,
+                           batterygraphwidget,
                            mylayoutbox[s],
                            s == 1 and mysystray or nil }
     mywibox[s].screen = s
